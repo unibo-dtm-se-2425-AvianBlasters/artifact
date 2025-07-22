fd071984b52030f0ca6fdfc2d519394b0db53994
Filippo Velli <filippo.velli@studio.unibo.it>
2025-07-22 18:16:01 +0200

    Renaming the template's project name with the one of our porject

--- my_project/__main__.py
+++ my_project/__main__.py
@@ -1,7 +1,7 @@
-import my_project
+import Avian_Blasters
 
 # this is the main module of your app
 # it is only required if your project must be runnable
-# this is the script to be executed whenever some users writes `python -m my_project` on the command line, eg.
-x = my_project.MyClass().my_method()
+# this is the script to be executed whenever some users writes `python -m Avian_Blasters` on the command line, eg.
+x = Avian_Blasters.MyClass().my_method()
 print(x)
