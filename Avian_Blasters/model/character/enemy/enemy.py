1676ce9631ea9511cd789851b96dd7a4cca05954
Mariam Baloch <mb05987@st.habib.edu.pk>
2025-08-10 19:09:58 +0200

    Updated enemy.py

--- Avian_Blasters/model/character/enemy/enemy.py
+++ Avian_Blasters/model/character/enemy/enemy.py
@@ -1,4 +1,8 @@
 from character import Character
 
 class Enemy(Character):
-    ...
+    """Abstract base class for all enemies in the game."""
+    
+    def attack (self):
+        """ It defines how the enemy attacks the player."""
+        raise NotImplementedError("This method should be overridden by subclasses.")
