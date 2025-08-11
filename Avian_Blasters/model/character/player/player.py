447f6cfeaf34e8ee7fa156e5f0e3d58fd12fa727
Filippo Velli <filippo.velli@studio.unibo.it>
2025-08-11 12:14:56 +0200

    feat!: added PlayerStatus to the Player interface and implementation

--- Avian_Blasters/model/character/player/player.py
+++ Avian_Blasters/model/character/player/player.py
@@ -1,5 +1,6 @@
 from model.character.character import Character
 from power_up_handler import PowerUpHandler
+from player_status_handler import PlayerStatus
 from score import Score
 
 class Player(Character):
@@ -8,4 +9,7 @@ class Player(Character):
         ...
 
     def get_score(self) -> Score:
+        ...
+    
+    def get_status(self) -> PlayerStatus:
         ...
