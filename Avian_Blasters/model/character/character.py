2344a23715d529ae5b6d3e35f4f73d4f33c9256b
Filippo Velli <filippo.velli@studio.unibo.it>
2025-08-14 14:48:20 +0200

    fix!: substitution of health variable with health_handler

--- Avian_Blasters/model/character/character.py
+++ Avian_Blasters/model/character/character.py
@@ -1,15 +1,11 @@
 from Avian_Blasters.model.entity import Entity
 from Avian_Blasters.model.item.projectile.projectile import Projectile
+from Avian_Blasters.model.character.health_handler import HealthHandler
 from typing import Optional
 
 class Character(Entity):
 
-    @property
-    def health(self) -> int:
-        ...
-
-    @health.setter
-    def health(self, health : int):
+    def get_health_handler(self) -> HealthHandler:
         ...
     
     def shoot(self) -> Optional[Projectile]:
--- Avian_Blasters/model/character/character_impl.py
+++ Avian_Blasters/model/character/character_impl.py
@@ -1,23 +1,17 @@
 from Avian_Blasters.model.character.character import Character
+from Avian_Blasters.model.character.health_handler import HealthHandler
+from Avian_Blasters.model.character.health_handler_impl import HealthHandlerImpl
 from Avian_Blasters.model.entity_impl import EntityImpl
 from Avian_Blasters.model.entity import Entity
 
 class CharacterImpl(EntityImpl, Character):
     def __init__(self, x : int, y : int, width : int, height : int, type : Entity.TypeArea, delta : int, health : int):
         super().__init__(x, y, width, height, type, delta)
-        self._health = health
+        self._health_handler = HealthHandlerImpl(health)
         self._max_health = health
     
-    @property
-    def health(self) -> int:
-        return self._health
-
-    @health.setter
-    def health(self, health : int):
-        if (self._health + health) < self._max_health:
-            self._health += health
-        else:
-            self._health = self._max_health
+    def get_health_handler(self) -> HealthHandler:
+        return self._health_handler
     
     def shoot(self):
         ...
