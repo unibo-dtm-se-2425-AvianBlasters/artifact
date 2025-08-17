e76b9363e07e618db097b42c22ef58e60868591e
Filippo Velli <filippo.velli@studio.unibo.it>
2025-08-17 14:12:22 +0200

    docs: added documentation for Character and its implementation

--- Avian_Blasters/model/character/character.py
+++ Avian_Blasters/model/character/character.py
@@ -4,9 +4,15 @@ from Avian_Blasters.model.character.health_handler import HealthHandler
 from typing import Optional
 
 class Character(Entity):
+    """Character is a type of Entity posessing health and being
+    able to attack by shooting a Projectile"""
 
     def get_health_handler(self) -> HealthHandler:
+        """Returns the HealthHandler of the character"""
         ...
     
     def shoot(self) -> Optional[Projectile]:
+        """The Character will shoot a projectile and will
+        return it if it does. If the recoil/cooldown is not
+        yet done, it will return nothing"""
         ...
--- Avian_Blasters/model/character/character_impl.py
+++ Avian_Blasters/model/character/character_impl.py
@@ -5,6 +5,8 @@ from Avian_Blasters.model.entity_impl import EntityImpl
 from Avian_Blasters.model.entity import Entity
 
 class CharacterImpl(EntityImpl, Character):
+    """CharacterImpl is an implementation of Character"""
+
     def __init__(self, x : int, y : int, width : int, height : int, type : Entity.TypeArea, delta : int, health : int):
         super().__init__(x, y, width, height, type, delta)
         self._health_handler = HealthHandlerImpl(health)
