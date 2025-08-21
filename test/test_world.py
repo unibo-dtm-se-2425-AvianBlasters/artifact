94127b771e8461e33e3ee96b01dfb8300d0db0e2
Filippo Velli <filippo.velli@studio.unibo.it>
2025-08-21 12:57:48 +0200

    chore: removed Direction import in World

--- test/test_world.py
+++ test/test_world.py
@@ -2,7 +2,6 @@ import unittest
 from Avian_Blasters.model.character.player import *
 from Avian_Blasters.model.entity import Entity
 from Avian_Blasters.model.world_impl import WorldImpl
-from Avian_Blasters.model.item.item import Direction
 from Avian_Blasters.model.item.projectile.projectile import ProjectileType
 from Avian_Blasters.model.item.projectile.projectile_impl import ProjectileImpl
 
@@ -10,9 +9,9 @@ class TestWorld(unittest.TestCase):
 
     def setUp(self):
         self.players = [PlayerImpl(0,0,10,10,1,3,0,1,100,-100), PlayerImpl(0,0,10,10,1,3,0,1,100,-100)]
-        self.projectiles = [ProjectileImpl(0,10,10,10,Entity.TypeArea.ENEMY_PROJECTILE,ProjectileType.NORMAL, Direction.UP, 1),
-                            ProjectileImpl(0,10,10,10,Entity.TypeArea.PLAYER_PROJECTILE,ProjectileType.NORMAL, Direction.DOWN, 1),
-                            ProjectileImpl(0,10,10,10,Entity.TypeArea.ENEMY_PROJECTILE,ProjectileType.NORMAL, Direction.UP, 1)]
+        self.projectiles = [ProjectileImpl(0,10,10,10,Entity.TypeArea.ENEMY_PROJECTILE,ProjectileType.NORMAL, 1),
+                            ProjectileImpl(0,10,10,10,Entity.TypeArea.PLAYER_PROJECTILE,ProjectileType.NORMAL, 1),
+                            ProjectileImpl(0,10,10,10,Entity.TypeArea.ENEMY_PROJECTILE,ProjectileType.NORMAL, 1)]
         self.entities = list()
         for i in self.players:
             self.entities.append(i)
