713b8a0c15755649f3230c3ac652ead1d02a7ddd
Filippo Velli <filippo.velli@studio.unibo.it>
2025-08-24 21:34:10 +0200

    feat: introduction of the CoolDownHandler interface and implementation

---
+++ Avian_Blasters/model/cooldown_handler_impl.py
@@ -0,0 +1,28 @@
+from Avian_Blasters.model.cooldown_handler import CoolDownHandler
+
+class CoolDownHandlerImpl(CoolDownHandler):
+    """CoolDownHandlerImpl is the main implementation of the
+    CoolDownHandler interface"""
+    
+    def __init__(self, cooldown : int, refresh_rate : int):
+        self._cooldown = cooldown
+        self._refresh_rate = refresh_rate
+
+    @property
+    def cooldown(self) -> int:
+        return self._cooldown
+    
+    @cooldown.setter
+    def cooldown(self, new_cooldown : int):
+        self._cooldown = max(self._cooldown, new_cooldown)
+    
+    @property
+    def refresh_rate(self) -> int:
+        return self._refresh_rate
+    
+    def update(self):
+        if not self.is_over():
+            self._cooldown -= 1
+    
+    def is_over(self) -> bool:
+        return self._cooldown <= 0
