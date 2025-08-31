516add72c83868c326c1f1a13f5ecef9d2fbc099
Filippo Velli <filippo.velli@studio.unibo.it>
2025-08-31 18:33:30 +0200

    feat(Menu): updated the Menu api. Added support to go through the scoreboard from the score sub-menu

--- Avian_Blasters/menu_impl.py
+++ Avian_Blasters/menu_impl.py
@@ -1,5 +1,4 @@
 from Avian_Blasters.menu import MainMenu
-from Avian_Blasters.controller.game_controller import GameController
 from Avian_Blasters.controller.game_controller_impl import GameControllerImpl
 from Avian_Blasters.scoreboard_impl import ScoreboardImpl
 import pygame
@@ -8,10 +7,6 @@ import pygame_menu.events
 from pygame_menu.widgets.widget.table import Table
 from tkinter import *
 from tkinter.ttk import *
-
-DIFFICULTY = [3]
-NAME = [""]
-FPS = [60]
 
 class MainMenuImpl(MainMenu):
 
@@ -31,33 +26,46 @@ class MainMenuImpl(MainMenu):
         self._SCORE_MENU = pygame_menu.Menu('Best Scores', self._width/2, self._height/2)
         self._table_number = 0
         self._table : Table = None
+        self._highest = 0
+        self._max = 300
+        self._fps = 60
+        self._name = ""
+        self._difficulty = 3
         
     def initiate(self):
         self._SCORE_MENU.add.button('Go Back', pygame_menu.events.BACK)
         self._table = self._SCORE_MENU.add.table(table_id=str(self._table_number), font_size=int(self._width*0.009))
-        self._build_table(self._table, 0, 300)
+        self._build_table(self._table, self._highest)
         
         self._MENU.add.text_input('Name :', default='', onchange=self._set_name)
         self._MENU.add.button('Play', self.play)
         self._MENU.add.selector('Difficulty :', [('Easy peasy', 3), ('Typical ride', 2), ('Hard as nails', 1)], default=0, onchange=self._set_difficulty)
         self._MENU.add.selector('FPS :', [('Smooth 60', 60), ('Typical 30', 30), ('Rough 15', 15)], default=0, onchange=self._set_fps)
         self._MENU.add.button('Scoreboard', self._SCORE_MENU)
-        self._MENU.add.button('Reset', self.reset_secoreboard, ScoreboardImpl(), self._table)
+        self._MENU.add.button('Reset', self._reset_secoreboard, self._table)
         self._MENU.add.button('Quit', pygame_menu.events.EXIT)
+        self._SCORE_MENU.add.button('Higher scores', self._build_table, self._table, -self._max)
+        self._SCORE_MENU.add.button('Lower scores', self._build_table, self._table, self._max)
+        self._reset_secoreboard(self._table)
         self._MENU.mainloop(self._surface)
 
-    def reset_secoreboard(self, scoreboard : ScoreboardImpl, table : Table):
-        scoreboard.reset_scoreboard()
-        self._build_table(table, 0, 300)
-        self._table._id = self._table_number
+    def _reset_secoreboard(self, table : Table):
+        ScoreboardImpl().reset_scoreboard()
+        self._build_table(table, 0)
         self._SCORE_MENU.render()
 
-    def _build_table(self, table : Table, beginning : int, end : int):
+    def _build_table(self, table : Table, beginning : int):
         scoreboard = ScoreboardImpl().get_scores(300)
         length = len(scoreboard)
+        
+        if self._highest + beginning <= 0:
+            beginning = 0
+            self._highest = 0
+        elif self._highest + beginning <= length:
+            self._highest += beginning
+        beginning = self._highest
 
-        if beginning > length:
-            return
+        print(self._highest)
 
         if len(table._rows) > 0:
             self._SCORE_MENU.remove_widget(str(self._table_number))
@@ -68,22 +76,27 @@ class MainMenuImpl(MainMenu):
         table.default_cell_padding = 6
         table.default_row_background_color = 'gray'
 
-        i = 1
-        if (end>length):
+        end = beginning + self._max
+        if end > length:
+            print(end)
             end = length
+
+        i = 1 
         for row in scoreboard[beginning:end]:
             name, score, difficulty = row
             table.add_row([str(i + beginning), name, str(score), difficulty], cell_align=pygame_menu.locals.ALIGN_LEFT)
             i += 1
-            if table.get_height() > self._SCORE_MENU.get_height() * 2/3:
+            if table.get_height() > self._SCORE_MENU.get_height() * 2/4:
                 break
+        if self._table_number == 0:
+            self._max = min(i - 1, self._max)
         table._max_height = (self._height / 3, True, True)
-        table.resize(width = table.get_width(), height = table.get_height())
+        self._SCORE_MENU.render()
     
     def play(self):
-        """Launches the game"""
-        controller = GameControllerImpl(DIFFICULTY[0], NAME[0], FPS[0])
+        controller = GameControllerImpl(self._difficulty, self._name, self._fps)
         if controller.initialize():
+            print("Game Start")
             controller.run()
         else:
             print("Failed to initialize game. Please check your pygame installation.")
@@ -91,17 +104,13 @@ class MainMenuImpl(MainMenu):
     def _set_difficulty(self, value : tuple[any, int], selection : any) -> None:
         selected, index = value
         print(f'Selected difficulty: "{selected}" ({selection}) at index {index}')
-        DIFFICULTY[0] = selection
+        self._difficulty = selection
 
     def _set_fps(self, value : tuple[any, int], selection : any) -> None:
         selected, index = value
         print(f'Selected difficulty: "{selected}" ({selection}) at index {index}')
-        FPS[0] = selection
+        self._fps = selection
 
-    def _set_name(name : str):
-        NAME[0] = name[:30]
-
-    def __are_there_still_elements(self, value: int):
-        scoreboard = ScoreboardImpl().get_scores(300)
-        length = len(scoreboard)
+    def _set_name(self, name : str):
+        self._name = name[:30]
 
