20ef3ac426ab501c5d3c03d6dfc0346623ce4e60
Beatrice Micolucci <beatrice.micolucci@studio.unibo.it>
2025-09-01 16:28:25 +0200

    feat: update move method in ProjectileImpl for Laser movement and add default projectile sizes in ProjectileFactory

--- Avian_Blasters/model/item/projectile/projectile_factory.py
+++ Avian_Blasters/model/item/projectile/projectile_factory.py
@@ -6,6 +6,12 @@ from Avian_Blasters.model.item.projectile.projectile_impl import ProjectileImpl
 
 class ProjectileFactory:
 
+    DEFAULT_SIZES = {
+        ProjectileType.NORMAL: (2, 4),
+        ProjectileType.LASER: (2, 90),
+        ProjectileType.SOUND_WAVE: (2, 5)
+    }
+
     def create_projectile(self, projectile_type: ProjectileType, x: int, y: int, width: int , height: int, type_area: Entity.TypeArea, delta: int = DEFAULT_DELTA):
         
         if not isinstance(type_area, Entity.TypeArea):
@@ -16,5 +22,10 @@ class ProjectileFactory:
         
         if width <= 0 or height <= 0:
             raise ValueError("Width and height must be positive integers")
+
+        if projectile_type in self.DEFAULT_SIZES:
+                width, height = self.DEFAULT_SIZES[projectile_type]
+        else:
+            raise ValueError("Unsupported projectile type")
         
         return ProjectileImpl(x, y, width, height, type_area, projectile_type, delta)
--- Avian_Blasters/model/item/projectile/projectile_impl.py
+++ Avian_Blasters/model/item/projectile/projectile_impl.py
@@ -1,3 +1,4 @@
+from Avian_Blasters.model.area_impl import AreaImpl
 from Avian_Blasters.model.entity import Entity
 from Avian_Blasters.model.item.item_impl import ItemImpl
 from Avian_Blasters.model.item.projectile.projectile import Projectile, ProjectileType
@@ -14,4 +15,7 @@ class ProjectileImpl(Projectile):
     
     def move(self, movement_x, movement_y, width, height):
         if self.active:
-            super().move(movement_x, movement_y, width, height)
+            if self._projectile_type == ProjectileType.LASER:
+                self._area = AreaImpl(movement_x, self.get_area().get_position_y, width, height)
+            else:
+                super().move(movement_x, movement_y, width, height)
