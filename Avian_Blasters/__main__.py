d0937e75a3971b7f40d82c60051b4e5a7a63da79
Filippo Velli <filippo.velli@studio.unibo.it>
2025-09-09 11:54:28 +0200

    refactor(main): removed controller as it is initialised inside the menu already

--- Avian_Blasters/__main__.py
+++ Avian_Blasters/__main__.py
@@ -1,15 +1,9 @@
-from Avian_Blasters.controller.game_controller_impl import GameControllerImpl, TARGET_FPS
 from Avian_Blasters.menu_impl import MainMenuImpl
 
 def main():
     """Main entry point for Avian Blasters game"""
     menu = MainMenuImpl()
     menu.initiate()
-    game_controller = GameControllerImpl(3, "Test", TARGET_FPS)
-    if game_controller.initialize():
-        game_controller.run()
-    else:
-        print("Failed to initialize game. Please check your pygame installation.")
 
 if __name__ == "__main__":
     main()
