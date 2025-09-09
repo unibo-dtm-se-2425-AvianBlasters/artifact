2214d8ff01f4a134a35d3c8217717abf1eb8925a
Filippo Velli <filippo.velli@studio.unibo.it>
2025-09-09 10:45:27 +0200

    refactor(sprite_manager_player): improved sprite selection logic considering power-ups

--- Avian_Blasters/view/sprite_manager_player.py
+++ Avian_Blasters/view/sprite_manager_player.py
@@ -3,6 +3,7 @@ from typing import Dict, Tuple, Optional
 from Avian_Blasters.view.abstract_sprite_manager import AbstractSpriteManager
 from Avian_Blasters.model.character.player import Player
 from Avian_Blasters.model.character.player.player_status_handler import PlayerStatus
+from Avian_Blasters.model.item.power_up.power_up import PowerUpType
 import os
 
 class SpriteManagerPlayer(AbstractSpriteManager):
@@ -44,12 +45,17 @@ class SpriteManagerPlayer(AbstractSpriteManager):
     
     def get_sprite(self, player : Player, variant: int = 0) -> pygame.Surface:
         image = player.get_health_handler().current_health
-        if player.is_hurt():
-            image = 0
-        elif player.get_status().status == PlayerStatus.Status.INVULNERABLE:
-            image = 5
-        elif player.get_power_up_handler().get_current_power_up() is not None:
-            image = 4
+        current_power_up = player.get_power_up_handler().get_current_power_up()
+        if player.get_status().status == PlayerStatus.Status.INVULNERABLE:
+            if player.is_hurt():
+                image = 0
+            else:
+                image = 5
+        elif current_power_up is not None:
+            if current_power_up.power_up_type == PowerUpType.INVULNERABILITY:
+                image = 5
+            elif current_power_up.power_up_type != PowerUpType.HEALTH_RECOVERY:
+                image = 4
         return super().get_sprite(image, variant)
     
     def get_sprite_size(self, index : int) -> Tuple[int, int]:
@@ -58,7 +64,7 @@ class SpriteManagerPlayer(AbstractSpriteManager):
     def is_loaded(self) -> bool:
         return super().is_loaded()
     
-    def _create_fallback_sprite(self, current_health : int) -> pygame.Surface:
+    def _create_fallback_sprite(self, index : int) -> pygame.Surface:
         colours = {
             5: (0, 0, 255),      # Blue
             4: (255, 69, 0),     # Orange
@@ -67,4 +73,4 @@ class SpriteManagerPlayer(AbstractSpriteManager):
             1: (255, 0, 0),      # Red
             0: (255, 255, 255)   # White 
         }
-        return super()._create_fallback_sprite(current_health, colours)
+        return super()._create_fallback_sprite(index, colours)
