6de59f51d16dd0f5eb767e3dd2a525dd15575474
Filippo Velli <filippo.velli@studio.unibo.it>
2025-09-12 17:01:15 +0200

    chore: removed unused default package classes

--- Avian_Blasters/__init__.py
+++ Avian_Blasters/__init__.py
@@ -7,9 +7,9 @@ logger = logging.getLogger('Avian_Blasters')
 # this is the initial module of your app
 # this is executed whenever some client-code is calling `import Avian_Blasters` or `from Avian_Blasters import ...`
 # put your main classes here, eg:
-class MyClass:
-    def my_method(self):
-        return "Hello World"
+# class MyClass:
+#    def my_method(self):
+#        return "Hello World"
 
 # let this be the last line of this file
 logger.info("Avian_Blasters loaded")
